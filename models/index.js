5a51fa506c83b66daf78d24442547ac52c045db9
Mazkid21 <zmaraziti@yahoo.com>
2017-07-25 14:49:23 -0600

    sprint 3 added

--- models/index.js
+++ models/index.js
@@ -2,4 +2,7 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/book-app");
 
 module.exports.Book = require("./book.js");
+module.exports.Character = require("./book.js");
 module.exports.Author = require("./author.js");
+
+
